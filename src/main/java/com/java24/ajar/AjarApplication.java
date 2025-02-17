0df0f18e6e8f7f7e063eeaaeaf49cff29f6e3074
Mohammad Abbas <msaabbas6@gmail.com>
2025-02-17 10:25:07 +0100

    first commit

---
+++ src/main/java/com/java24/ajar/AjarApplication.java
@@ -0,0 +1,13 @@
+package com.java24.ajar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AjarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java24/ajar/AjarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java24.ajar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
