b066ab0c1ddd89c9d520d1aa66e5bbf077979fd0
Your GitHub Username <alomar.hasan@hotmail.com>
2025-03-08 01:48:26 +0100

    mplement Property Search Functionality by Location and Price**

--- src/main/java/com/java24/ajar/Repositories/PlaceRepository.java
+++ src/main/java/com/java24/ajar/Repositories/PlaceRepository.java
@@ -1,8 +1,22 @@
 package com.java24.ajar.Repositories;
 
 import com.java24.ajar.models.Place;
+import org.springframework.data.domain.Page;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+import java.math.BigDecimal;
+import java.util.List;
 
 public interface PlaceRepository extends MongoRepository<Place, String> {
 
-}
+
+    Page<Place> findByPlaceAndPriceBetween(
+        String city,
+        BigDecimal minPrice,
+        BigDecimal maxPrice);
+    List<Place> FindBYPriceBetween(BigDecimal minPrice, BigDecimal maxPrice);
+
+    }
+
